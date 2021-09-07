7d516a45145d14f4e94201e2e56d93eb8ce7a6d5
Storm Søndergaard <stormsoendergaard@gmail.com>
2021-09-07 11:41:25 +0200

    Working UDP, use package sender to test

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
 
 namespace Client
 {
@@ -6,7 +12,61 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UDP udp = new UDP(11000);
+            Console.WriteLine("Client...");
+
+
+            while (true) { /* This "idling" thing not very optimal... */ }
+        }
+    }
+
+    public class Package { }
+
+    public class UDP
+    {
+        private static UdpClient m_UDPListener = null;
+
+        public UDP(int port)
+        {
+            m_UDPListener = new UdpClient(port);
+            m_UDPListener.BeginReceive(new AsyncCallback(ReceiveData), null);
+        }
+
+        public void SendData(IPEndPoint endPoint)
+        {
+            string tmpData = "data";
+
+            try
+            {
+                if (endPoint != null)
+                {
+                    byte[] bytes = Encoding.ASCII.GetBytes(tmpData);
+                    m_UDPListener.BeginSend(bytes, bytes.Length, endPoint, null, null);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Error sending data to {endPoint} via UDP: {exception}");
+            }
+        }
+
+        public void ReceiveData(IAsyncResult asyncResult)
+        {
+            try
+            {
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = m_UDPListener.EndReceive(asyncResult, ref remoteEndPoint);
+
+                // Start listening for new packages
+                m_UDPListener.BeginReceive(new AsyncCallback(ReceiveData), null);
+
+                // Handle data
+                Console.WriteLine(Encoding.UTF8.GetString(data));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Error receiving UDP data: {exception}");
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -6,7 +6,7 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Server...");
         }
     }
 }
